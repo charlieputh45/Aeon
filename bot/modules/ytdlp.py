71d91386630d507b464255efb72ce613d109d91b
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2023-12-31 19:21:45 +0530

    Update ytdlp.py

--- bot/modules/ytdlp.py
+++ bot/modules/ytdlp.py
@@ -450,7 +450,6 @@ async def _ytdl(client, message, isLeech=False, sameDir=None, bulk=[]):
             options[key] = value
 
         options['playlist_items'] = '0'
-        options["compat_opts"] = {"prefer-legacy-http-handler": "urllib"}
 
     try:
         result = await sync_to_async(extract_info, link, options)
